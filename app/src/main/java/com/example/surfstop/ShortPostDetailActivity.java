b5fd7aec462c95ec7f39aa07da7540ac53b0f7a2
Christiana Marchese <christianamarc@fb.com>
2022-06-30 15:56:45 -0700

    Basic layout done

---
+++ app/src/main/java/com/example/surfstop/ShortPostDetailActivity.java
@@ -0,0 +1,14 @@
+package com.example.surfstop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ShortPostDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_short_post_detail);
+    }
+}
